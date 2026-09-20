0a741ba5f9e839e367bb920ae12b8c58fa931a7e
Hema9500 <rajahemalatha335@gmail.com>
2026-09-20 20:28:42 +0530

    Add files via upload

---
+++ AccessiblePortfolio/js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.querySelector("form");
+
+    if (form) {
+
+        form.addEventListener("submit", function (event) {
+
+            event.preventDefault();
+
+            alert("Thank you! Your message has been submitted.");
+
+        });
+
+    }
+
+});
